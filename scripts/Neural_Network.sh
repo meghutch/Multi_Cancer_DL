5a4774f03ee184cda10e10769a4433ab73728c76
Meghan Hutch <mrh1996@quser24.quest.it.northwestern.edu>
2020-02-18 08:14:42 -0600

    add quest job script

---
+++ scripts/Neural_Network.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH -A p31049               # Allocation
+#SBATCH -p short                # Queue
+#SBATCH -t 02:00:00             # Walltime/duration of the job
+#SBATCH -N 1                    # Number of Nodes
+#SBATCH --mem=8G               # Memory per node in GB needed for a job. Also see --mem-per-cpu
+#SBATCH --ntasks-per-node=1     # Number of Cores (Processors)
+#SBATCH --mail-user=,meghutch@u.northwestern.edu  # Designate email address for job communications
+#SBATCH --mail-type=FAIL     # Events options are job BEGIN, END, NONE, FAIL, REQUEUE
+#SBATCH --job-name="Neural_Network"       # Name of job
+
+# unload any modules that carried over from your command line session
+module purge
+
+# load modules you need to use
+module load python/anaconda3.6
+module load java
+
+source activate envs_dl
+
+# A command you actually want to execute:
+
+python Neural_Network.py

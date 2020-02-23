8c5682111bba611c7c997346c1ff18f6d7735675
Meghan Hutch <mrh1996@quser21.quest.it.northwestern.edu>
2020-02-23 10:34:58 -0600

    add batch submission script

--- scripts/Neural_Network.sh
+++ scripts/Neural_Network.sh
@@ -3,11 +3,12 @@
 #SBATCH -p short                # Queue
 #SBATCH -t 02:00:00             # Walltime/duration of the job
 #SBATCH -N 1                    # Number of Nodes
-#SBATCH --mem=8G               # Memory per node in GB needed for a job. Also see --mem-per-cpu
+#SBATCH --mem=10G               # Memory per node in GB needed for a job. Also see --mem-per-cpu
 #SBATCH --ntasks-per-node=1     # Number of Cores (Processors)
 #SBATCH --mail-user=,meghutch@u.northwestern.edu  # Designate email address for job communications
 #SBATCH --mail-type=FAIL     # Events options are job BEGIN, END, NONE, FAIL, REQUEUE
-#SBATCH --job-name="Neural_Network"       # Name of job
+#SBATCH --job-name="Neural_Network"       # Name of job 
+
 
 # unload any modules that carried over from your command line session
 module purge
@@ -20,4 +21,4 @@ source activate envs_dl
 
 # A command you actually want to execute:
 
-python Neural_Network.py
+python Neural_Network.py > /projects/p31049/Multi_Cancer_DL/04_Results/HL100_50_25_LR_001_E100.out
